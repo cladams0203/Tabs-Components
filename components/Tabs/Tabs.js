cb5936614049d3456e6e71503a6958c6cd13f1e0
cladams <cladams0203@gmail.com>
2019-05-09 20:31:35 -0400

    removed console logs and cleaned up code.  mvp finished

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,11 +6,10 @@ class TabLink {
     
     // Get the custom data attribute on the Link
     this.data = this.element.dataset.tab;
-    // console.log(this.data);
     
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
-    // console.log(this.itemElement);
+
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
     
@@ -28,7 +27,6 @@ class TabLink {
   //   // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
       Array.from(links).forEach((e) => { 
       e.classList.remove('tabs-link-selected');
-      console.log(e);
     });
 
   //   // Add a class named "tabs-link-selected" to this link
@@ -44,13 +42,12 @@ class TabItem {
   constructor(item) {
     // Assign this.element to the passed in element
     this.item = item;
-    console.log(this.item);
   }
 
   select() {
      // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll('.tabs-item');
-    
+
      // Remove the class "tabs-item-selected" from each element
     Array.from(items).forEach((e) => {
       e.classList.remove('tabs-item-selected');
